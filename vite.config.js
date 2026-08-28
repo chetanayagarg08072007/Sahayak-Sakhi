1589f8226296b5def1db40a762a4bd4b875c4032
Chetanaya Garg <chetanaya.care2007@gmail.com>
2026-08-28 15:06:33 +0530

    Update Sahayak Sakhi

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/family-support-app/",
+  base: "/Sahayak-Sakhi/",
   plugins: [react()],
 });
